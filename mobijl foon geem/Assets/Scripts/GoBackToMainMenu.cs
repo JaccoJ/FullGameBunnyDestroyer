641b023690bbc63f20d98a705165617deb86a292
JaccoJongenotter <22053@ma-web.nl>
2017-11-22 13:14:27 +0100

    scripts

---
+++ mobijl foon geem/Assets/Scripts/GoBackToMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBackToMainMenu : MonoBehaviour {
+
+ 
+    public float times = 1f;
+
+
+    void Update()
+    {
+        times -= Time.deltaTime;
+        if (Input.touchCount == 1  && times <= 0|| Input.GetMouseButtonDown(0) && times <= 0)
+        {
+            Application.LoadLevel(0);
+        }
+        if (times <= 0)
+        {
+            times = 0;
+        }
+    }
+}
