68894b623c8f6ff13c06b46931fbe5f5076d7a9f
JaccoJongenotter <22053@ma-web.nl>
2017-11-22 12:31:04 +0100

    Full Game

---
+++ mobijl foon geem/Library/Collab/Base/Assets/Scripts/GoBackToMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBackToMainMenu : MonoBehaviour {
+
+
+
+    void Update()
+    {
+        if (Input.touchCount == 1 || Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
